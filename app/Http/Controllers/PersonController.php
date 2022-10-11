a20e562e65e33025c6a9ffd44765f012c80f8e4a
Victor Willhuber <victorwillhuber@gmail.com>
2022-10-11 17:44:02 -0300

    [FEATURE] First Person Model.
    Thorougher with Factory, seeding, controller, routing, requests and db migration (people CRUD)

---
+++ app/Http/Controllers/PersonController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePersonRequest;
+use App\Http\Requests\UpdatePersonRequest;
+use App\Models\Person;
+use Response;
+
+class PersonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Person::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorePersonRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePersonRequest $request)
+    {
+        $person = Person::create($request->all());
+        return $person;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Person  $person
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Person $person)
+    {
+        return $person;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatePersonRequest  $request
+     * @param  \App\Models\Person  $person
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatePersonRequest $request, Person $person)
+    {
+        try
+        {
+            $person->update($request->all());
+            return $person;
+
+        }catch(QueryException $ex) {
+            return ['success'=>false, 'error'=>$ex->getMessage()];
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Person  $person
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Person $person)
+    {
+        $person->delete();
+        return response('Deleted', 200)->header('Content-Type', 'text/plain');
+    }
+}
---
+++ database/factories/PersonFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Person>
+ */
+class PersonFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'phone' => $this->faker->e164PhoneNumber(),
+            'birth_date' => $this->faker->date($format = 'Y-m-d', $min = 'now'),
+        ];
+    }
+}
